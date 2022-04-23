6f4cbb2c3bb53bb57eda8e434f2b4c96fa6282c1
purepelmen <dan.fedotov45@gmail.com>
2022-04-23 22:15:18 +0300

    StoppedReason constants reordering

--- Assets/NetFork/StoppedReason.cs
+++ Assets/NetFork/StoppedReason.cs
@@ -1,8 +1,9 @@
 ﻿public enum StoppedReason : uint
 {
     LocalStopped,
+    Timeout,
+
     RemotelyDisconnected = 0xF0,
     ServerClosing = 0xF1,
-    RemoteError = 0xF2,
-    Timeout
+    RemoteError = 0xF2
 }
